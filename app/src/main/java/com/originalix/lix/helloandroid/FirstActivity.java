70628e52276a3aee21fcddc3277beb5a5890c0ce
Lix <xiao.liunit@gmail.com>
2017-07-31 22:18:07 +0800

    first layout

--- app/src/main/java/com/originalix/lix/helloandroid/FirstActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/FirstActivity.java
@@ -8,6 +8,7 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
     }
 
 }
