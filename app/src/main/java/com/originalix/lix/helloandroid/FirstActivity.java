a06c0fa6acacc072983b6cdaa82fd2bce0c512d3
Lix <xiao.liunit@gmail.com>
2017-07-31 22:09:11 +0800

    create first activity

--- app/src/main/java/com/originalix/lix/helloandroid/FirstActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/FirstActivity.java
@@ -2,16 +2,12 @@ package com.originalix.lix.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.e("xyz->HelloAndroidActivity", "onCreate execute");
     }
-
 
 }
