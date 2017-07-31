5ec5b4a60670107cc4dedac18893181034354251
Lix <xiao.liunit@gmail.com>
2017-07-31 21:46:01 +0800

    chang appname

--- app/src/main/java/com/originalix/lix/helloandroid/MainActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.originalix.lix.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
