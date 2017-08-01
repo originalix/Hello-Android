8cd829cf6df4341237f0c8572b38af54efbabead
Lix <xiao.liunit@gmail.com>
2017-08-01 10:19:48 +0800

    hide action title  bar

--- app/src/main/java/com/originalix/lix/helloandroid/MainActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/MainActivity.java
@@ -10,10 +10,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /* 因为继承自AppCompatActivity 所以下面方法无效*/
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        /* 换此方法隐藏标题栏 */
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         setContentView(R.layout.activity_main);
         Log.e("xyz->HelloAndroidActivity", "onCreate execute");
     }
-
 
 }
