973ac34f69e88701392997b802cf781373afa648
Lix <xiao.liunit@gmail.com>
2017-08-03 08:23:56 +0800

    add button click

--- app/src/main/java/com/originalix/lix/helloandroid/LifeCycleActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/LifeCycleActivity.java
@@ -1,13 +1,39 @@
 package com.originalix.lix.helloandroid;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
-public class LifeCycleActivity extends AppCompatActivity {
+public class LifeCycleActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_cycle);
+        Log.d("lix", "onCreate");
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_life_cycle);
+        Button startNormalActivity = (Button) findViewById(R.id.start_normal_activity);
+        Button startDialogActivity = (Button) findViewById(R.id.start_dialog_activity);
+        startNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LifeCycleActivity.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        startDialogActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LifeCycleActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
