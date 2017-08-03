aefcd693f488afec1d9bc540524b81ceb96f800f
Lix <xiao.liunit@gmail.com>
2017-08-03 16:54:39 +0800

    show dialog activity

--- app/src/main/java/com/originalix/lix/helloandroid/DialogActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/DialogActivity.java
@@ -1,9 +1,10 @@
 package com.originalix.lix.helloandroid;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DialogActivity extends AppCompatActivity {
+public class DialogActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
