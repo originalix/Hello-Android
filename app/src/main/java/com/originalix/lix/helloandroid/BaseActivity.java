1f05f55cc82f792ecb031d32840de46cd0eb7edf
Lix <xiao.liunit@gmail.com>
2017-08-03 20:58:50 +0800

    create activity collector for manage activity

--- app/src/main/java/com/originalix/lix/helloandroid/BaseActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/BaseActivity.java
@@ -2,12 +2,14 @@ package com.originalix.lix.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("BaseActivity", getClass().getSimpleName());
         setContentView(R.layout.activity_base);
     }
 }
