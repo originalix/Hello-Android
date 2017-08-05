0a1cc60c4fecdd138e7673ec2d6ba2c5ba12d8a0
Lix <xiao.liunit@gmail.com>
2017-08-05 10:22:44 +0800

    add title button click function

--- app/src/main/java/com/originalix/lix/helloandroid/TitileLayout.java
+++ app/src/main/java/com/originalix/lix/helloandroid/TitileLayout.java
@@ -1,10 +1,14 @@
 package com.originalix.lix.helloandroid;
 
+import android.app.Activity;
 import android.content.Context;
 import android.text.Layout;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.originalix.lix.helloandroid.R;
 
@@ -17,5 +21,21 @@ public class TitileLayout extends LinearLayout {
     public TitileLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.title, this);
+        Button titleBack = (Button) findViewById(R.id.title_back);
+        Button titleEdit = (Button) findViewById(R.id.title_edit);
+
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity) getContext()).finish();
+            }
+        });
+
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "You clicked edit button", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
