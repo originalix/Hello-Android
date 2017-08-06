93914adf7de9f3a9ab8bb197063bdd1ee360340e
Lix <xiao.liunit@gmail.com>
2017-08-06 17:18:49 +0800

    碎片新闻节目布局

---
+++ app/src/main/java/com/originalix/lix/helloandroid/NewsContentActivity.java
@@ -0,0 +1,32 @@
+package com.originalix.lix.helloandroid;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsContentActivity extends AppCompatActivity {
+
+    public static void actionStart(Context context, String newsTitle, String newsContent) {
+        Intent intent = new Intent(context, NewsContentActivity.class);
+        intent.putExtra("news_title", newsTitle);
+        intent.putExtra("news_content", newsContent);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_content);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        String newsTitle = getIntent().getStringExtra("news_title");
+        String newsContent = getIntent().getStringExtra("news_content");
+        NewsContentFragment newsContentFragment = (NewsContentFragment) getFragmentManager().findFragmentById(R.id.news_content_fragment);
+        newsContentFragment.refresh(newsTitle, newsContent);
+    }
+}
---
+++ app/src/main/java/com/originalix/lix/helloandroid/NewsContentFragment.java
@@ -0,0 +1,37 @@
+package com.originalix.lix.helloandroid;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by Lix on 2017/8/6.
+ */
+
+public class NewsContentFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.news_content_frag, container, false);
+        return view;
+    }
+
+    public void refresh(String newsTitle, String newsContent) {
+        View visibilityLayout = view.findViewById(R.id.visibility_layout);
+        visibilityLayout.setVisibility(View.VISIBLE);
+        TextView newsTitleText = (TextView) view.findViewById(R.id.news_title);
+        TextView newsContentText = (TextView) view.findViewById(R.id.news_content);
+        newsTitleText.setText(newsTitle);
+        newsContentText.setText(newsContent);
+    }
+
+
+}
