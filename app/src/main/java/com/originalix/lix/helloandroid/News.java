6243f0ef5db1835d52472815ea026221f085cd31
Lix <xiao.liunit@gmail.com>
2017-08-06 17:00:30 +0800

    创建新闻列表代码

---
+++ app/src/main/java/com/originalix/lix/helloandroid/News.java
@@ -0,0 +1,26 @@
+package com.originalix.lix.helloandroid;
+
+/**
+ * Created by Lix on 2017/8/6.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/com/originalix/lix/helloandroid/NewsAdapter.java
@@ -0,0 +1,41 @@
+package com.originalix.lix.helloandroid;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Lix on 2017/8/6.
+ */
+
+public class NewsAdapter extends ArrayAdapter<News> {
+    private int resourceId;
+
+    public NewsAdapter(Context context, int textViewResourceId, List<News> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        News news = getItem(position);
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+        } else {
+            view = convertView;
+        }
+
+        TextView newsTitileText = (TextView) view.findViewById(R.id.news_title);
+        newsTitileText.setText(news.getTitle());
+        return view;
+    }
+}
