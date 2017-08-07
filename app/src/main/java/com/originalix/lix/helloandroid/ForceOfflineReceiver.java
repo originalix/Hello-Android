67b4519e1b81360b9cc5b64dce7c185f321bea4e
Lix <xiao.liunit@gmail.com>
2017-08-07 10:30:00 +0800

    create force offline extends broadcastreceiver

---
+++ app/src/main/java/com/originalix/lix/helloandroid/ForceOfflineReceiver.java
@@ -0,0 +1,35 @@
+package com.originalix.lix.helloandroid;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Lix on 2017/8/7.
+ */
+
+public class ForceOfflineReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        dialogBuilder.setTitle("Warning");
+        dialogBuilder.setMessage("You are forced to be offline. Please try to login argin.");
+        dialogBuilder.setCancelable(false);
+        dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ActivityCollector.finishAll();
+                Intent intent = new Intent(context, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        alertDialog.show();
+    }
+}
