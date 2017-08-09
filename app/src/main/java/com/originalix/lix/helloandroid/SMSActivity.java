d759ed2c085cca0f490523696bed33f60b06cf56
Lix <xiao.liunit@gmail.com>
2017-08-09 08:15:34 +0800

    message receiver

--- app/src/main/java/com/originalix/lix/helloandroid/SMSActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/SMSActivity.java
@@ -1,13 +1,44 @@
 package com.originalix.lix.helloandroid;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.TextView;
 
 public class SMSActivity extends AppCompatActivity {
+
+    private TextView sender;
+    private TextView content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
+        sender = (TextView) findViewById(R.id.sender);
+        content = (TextView) findViewById(R.id.content);
+    }
+
+    class MessageReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            SmsMessage[] messages = new SmsMessage[pdus.length];
+            for (int i = 0; i < messages.length; i++) {
+                messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+            }
+
+            String address = messages[0].getOriginatingAddress();
+            String fullMessage = "";
+            for (SmsMessage message : messages) {
+                fullMessage += message.getMessageBody();
+            }
+
+            sender.setText(address);
+            content.setText(fullMessage);
+        }
     }
 }
