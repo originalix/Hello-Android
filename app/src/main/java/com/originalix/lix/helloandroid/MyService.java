f6aa0301a221fcc7952632383d739df3b82c2f9c
Lix <xiao.liunit@gmail.com>
2017-08-10 10:44:36 +0800

    write myservice lifecycle

--- app/src/main/java/com/originalix/lix/helloandroid/MyService.java
+++ app/src/main/java/com/originalix/lix/helloandroid/MyService.java
@@ -1,6 +1,10 @@
 package com.originalix.lix.helloandroid;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
 
 /**
  * Created by Lix on 2017/8/10.
@@ -8,5 +12,24 @@ import android.app.Service;
 
 public class MyService extends Service {
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, @IntDef(value = {Service.START_FLAG_REDELIVERY, Service.START_FLAG_RETRY}, flag = true) int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 }
