fd3c0beade623749a9bb35119158a0a03fc06a73
Lix <xiao.liunit@gmail.com>
2017-08-12 20:27:27 +0800

    添加百度地图

---
+++ app/src/main/java/com/originalix/lix/helloandroid/MapActivity.java
@@ -0,0 +1,30 @@
+package com.originalix.lix.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.map.MapView;
+
+public class MapActivity extends AppCompatActivity {
+
+    private BMapManager manager;
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        manager = ginew BMapManager(this);
+        manager.init("cG5Z3yZXY72VTStyZ01n2HRERwN9GQWW", null);
+        setContentView(R.layout.activity_map);
+        mapView = (MapView) findViewById(R.id.map_view);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (manager  != null) {
+
+        }
+    }
+}
