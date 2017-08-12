96d21f82b897a0fbfe9596a94ec828da71eccf68
Lix <xiao.liunit@gmail.com>
2017-08-12 19:22:41 +0800

    location for myself

--- app/src/main/java/com/originalix/lix/helloandroid/LocatioActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/LocatioActivity.java
@@ -1,13 +1,83 @@
 package com.originalix.lix.helloandroid;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.Provider;
+import java.util.List;
 
 public class LocatioActivity extends AppCompatActivity {
+
+    private TextView positionTextView;
+
+    private LocationManager locationManager;
+
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locatio);
+
+        positionTextView = (TextView) findViewById(R.id.position_text_view);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // 获取所有可用的位置提供器
+        List<String> providerList = locationManager.getProviders(true);
+        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else if (provider.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = locationManager.NETWORK_PROVIDER;
+        } else {
+            Toast.makeText(this, "No location provider to use", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(Provider);
+        if (location != null) {
+            showLocation();
+        }
+
+        locationManager.requestLocationUpdates(provider, 5000, 1, locationManager);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+            locationManager.removeUpdates(locationManager);
+        }
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private void showLocation(Location location) {
+        String currentPosition = "latitude is " + location.getLatitude() + "\n" + "longitude is " + location.getLongitude();
+        positionTextView.setText(currentPosition);
     }
 }
