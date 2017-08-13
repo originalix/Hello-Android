9650d09bd518dfc6d887b0ee6d7938a237cc1245
Lix <xiao.liunit@gmail.com>
2017-08-13 10:42:23 +0800

    Sensor manager and listener

--- app/src/main/java/com/originalix/lix/helloandroid/LightSensorActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/LightSensorActivity.java
@@ -1,13 +1,47 @@
 package com.originalix.lix.helloandroid;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LightSensorActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private TextView lightLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_sensor);
+        lightLevel = (TextView) findViewById(R.id.light_level);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(listener);
+        }
+    }
+
+    private SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float value = sensorEvent.values[0];
+            lightLevel.setText("Current light level is " + value + "lx");
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
     }
 }
