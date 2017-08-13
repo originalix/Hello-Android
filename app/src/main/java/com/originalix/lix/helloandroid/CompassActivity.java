013f2b00ac25cba6125a4f77002c7ad7552db81f
Lix <xiao.liunit@gmail.com>
2017-08-13 11:05:48 +0800

    make easy compass

--- app/src/main/java/com/originalix/lix/helloandroid/CompassActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/CompassActivity.java
@@ -1,13 +1,63 @@
 package com.originalix.lix.helloandroid;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CompassActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+        textView = (TextView) findViewById(R.id.text_view);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(listener, magneticSensor, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(listener, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(listener);
+        }
+    }
+
+    private SensorEventListener listener = new SensorEventListener() {
+
+        float[] accelerometerValues = new float[3];
+        float[] magneticValues = new float[3];
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                accelerometerValues = sensorEvent.values.clone();
+            } else if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                magneticValues = sensorEvent.values.clone();
+            }
+
+            float[] R = new float[9];
+            float[] values = new float[3];
+
+            SensorManager.getRotationMatrix(R, null, accelerometerValues, magneticValues);
+            SensorManager.getOrientation(R, values);
+            textView.setText("value[0] is " + Math.toDegrees(values[0]));
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
 }
