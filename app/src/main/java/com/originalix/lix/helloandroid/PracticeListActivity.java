4ef1604bfbca2376d6d8d2a39bab46b75658a846
Lix <xiao.liunit@gmail.com>
2017-10-17 15:28:44 +0800

    test list view

--- app/src/main/java/com/originalix/lix/helloandroid/PracticeListActivity.java
+++ app/src/main/java/com/originalix/lix/helloandroid/PracticeListActivity.java
@@ -2,12 +2,41 @@ package com.originalix.lix.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class PracticeListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PracticeListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView listView;
+    private List<String> dataList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_list);
+        dataList = new ArrayList<String>();
+        for (int i = 0; i < 20; i++) {
+            dataList.add("第" + i + "条数据");
+        }
+
+        ListAdapter adapter = new ArrayAdapter<String>(PracticeListActivity.this, android.R.layout.simple_list_item_1, dataList);
+        listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+        Toast.makeText(PracticeListActivity.this, "点击了第" + position + "条数据", 0).show();
     }
 }
